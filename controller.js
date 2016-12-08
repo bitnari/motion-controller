a3bb6742e0e62680a388f0ed24a3682715831407
HelloWorld017 <deu07115@gmail.com>
2016-12-08 23:54:51 +0900

    Fixed bugs

--- controller.js
+++ controller.js
@@ -7,38 +7,42 @@ const cache = {};
 const generateToken = (len) => generate(len, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 class Controller{
 	static bindSocket(socket){
-		users[socket.id()] = {
+		users[socket.id] = {
 			socket,
 			token: undefined
 		};
 
 		socket.on('generate token', () => {
 			let token = generateToken(5);
-			(users[socket.id()] || {}).token = socket.token;
+			(users[socket.id] || {}).token = socket.token;
 			socket.emit('generate token', token);
 		});
 
 		socket.on('bind device', (token) => {
 			if(typeof token !== 'string') return;
-			Object.keys(users).map((k) => v).forEach((v) => {
+			let res = Object.keys(users).map((k) => v).every((v) => {
 				if(v.token === token){
-					v.bind = socket.id();
-				}
+					v.bind = socket.id;
+					users[socket.id].bind = v.socket.id;
 
-				users[socket.id()].bind = v.socket.id();
+					return false;
+				}
+				return true;
 			});
+
+			socket.emit('bind device', res);
 		});
 
 		socket.on('event', (e) => {
-			let bind = users[socket.id()].bind;
+			let bind = users[socket.id].bind;
 			if(bind && users[bind]){
 				((users[bind].socket || {}).emit || (() => {}))('event', e);
 			}
 		})
 
 		socket.on('disconnect', () => {
-			users[socket.id()] = undefined;
-			delete users[socket.id()];
+			users[socket.id] = undefined;
+			delete users[socket.id];
 		});
 	}
 
@@ -47,7 +51,7 @@ class Controller{
 		router.get('/', (req, res, next) => {
 			let device = 'desktop';
 
-			md = new MobileDetect(req.header('User-Agent'));
+			let md = new MobileDetect(req.header('User-Agent'));
 			if(md.mobile() !== null){
 				device = 'mobile';
 			}
